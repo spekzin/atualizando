12224bdbc65252d7838fa6972d79c0fdebca676f
spekzin <gabrielallanlopws@gmail.com>
2024-03-19 08:04:10 -0300

    19/03

---
+++ main.js
@@ -0,0 +1,15 @@
+
+document.getElementById("botaoConfirmar").addEventListener("click", function() {
+    const corSelecionada = document.getElementById("Cor").value;
+    const anoSelecionado = document.getElementById("Ano").value;
+    const modeloSelecionado = document.getElementById("Modelo").value;
+    const aviso = document.getElementById("aviso");
+
+    if (corSelecionada === "Cor" || anoSelecionado === "Ano" || modeloSelecionado === "Modelo") {
+        event.preventDefault(); // Evita o envio do formulário
+        aviso.style.display = "block"; // Mostra o aviso
+    } else {
+        window.location.href = asdasd.html;
+
+    }
+});
